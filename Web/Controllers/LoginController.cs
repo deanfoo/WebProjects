a307539a90fecf002830482671549a099e876b4a
deanfoo <215595979@qq.com>
2017-08-29 16:36:09 +0800

    33

---
+++ Web/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult DoLogin()
+        {
+            return View();
+        }
+
+    }
+}
